a534f197dad137e42dd8457166ef3daaf4825a0d
JeremyRodrigoGuerroAlejos <44576596+JeremyRodrigoGuerroAlejos@users.noreply.github.com>
2018-11-16 09:27:03 -0500

    Create appy.py

---
+++ appy.py
@@ -0,0 +1,25 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+#No tocar
+from flask import Flask
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+#No tocar 
+
+#importar nuestros routes de routes.py
+from routes import *
+
+
+#Inicia el server
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
